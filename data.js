4c9212fe8ddf46e8cc2fb829b7c4ab994398776a
giu3337 <72093755+giu3337@users.noreply.github.com>
2023-04-13 19:18:43 -0500

    add the data.js file

---
+++ data.js
@@ -0,0 +1,28 @@
+// Restaurant App 
+
+export const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        price: 14,
+        emoji: "🍕",
+        image: `images/pizza.png`,
+        id: 0,
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        image: `images/hamburger.png`,
+        id: 1,
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 14,
+        emoji: "🍺",
+        image: `images/beer.png`,
+        id: 2,
+    }
+]
